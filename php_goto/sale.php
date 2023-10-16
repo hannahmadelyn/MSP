7eca3e9e75ffe1fe3cf6517e52ac4600e189842c
Yeojin Song <41974994+Yeojinsong@users.noreply.github.com>
2023-10-16 13:07:31 +1100

    Add files via upload

--- php_goto/sale.php
+++ php_goto/sale.php
@@ -18,9 +18,40 @@ include "includes/head.php";
 <main>
 	<h1>Sale Dashboard</h1>
 	<section class="card">
+		<?php
+	// connect to the database
+	include "includes/connect.php";
+	
+	// write a sql statement to get data from the tbl_product table
+	try {
+		//create our SQL statment
+		$sql = "SELECT s.orderDate, COUNT(*) as totalOrders
+		FROM SALES s
+		JOIN SALEDETAILS sd ON s.transactionID = sd.transactionID
+		GROUP BY s.orderDate;";
+		
+		//excute the SQL statment and store the output
+		$resultSet = $pdo->query($sql);						
+	}//end of try block
+	
+	// what to do if sql statement fails
+	catch(PDOException $e){
+		// create an error message with excepiton details
+		echo "Error fetching products:".$e->getMessage();
+		
+		// stop script from contiuning
+		exit();						
+	}//end of catch block	
+
+	// extract data from the result set row by row
+	foreach($resultSet as $row){
+		// store row data in variables
+		$orderDate = $row['orderDate'];
+		$totalOrders = $row['totalOrders'];	}
+	?>
 		<article>
 			<h2>new Sale</h2>
-			<p>300</p>
+			<p><?echo $totalOrders;?></p>
 		</article>
 		<article>
 			<h2>total Sale</h2>
